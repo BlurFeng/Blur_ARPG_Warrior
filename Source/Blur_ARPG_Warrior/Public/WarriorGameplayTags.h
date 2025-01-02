a88aa358942609d534e8287a3055306061287319
Blur <pop297197228@163.com>
2025-01-02 21:32:29 +0900

    【AI】 Enemy BT logic update.

--- Source/Blur_ARPG_Warrior/Public/WarriorGameplayTags.h
+++ Source/Blur_ARPG_Warrior/Public/WarriorGameplayTags.h
@@ -53,6 +53,7 @@ namespace WarriorGameplayTags
 	BLUR_ARPG_WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Weapon);
 
 	BLUR_ARPG_WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Status_Strafing);
+	BLUR_ARPG_WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Status_UnderAttack);
 
 
 	//** Shared tags **//
