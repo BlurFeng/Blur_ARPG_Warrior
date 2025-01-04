25795cab85a21ee3fc71815ceb3a10fdcea14eeb
Blur <pop297197228@163.com>
2025-01-04 11:40:18 +0900

    【Enemy】  Boss Frost Giant. Hit React and Death. Remove Boss bar UI on dead.

--- Source/Blur_ARPG_Warrior/Private/Components/UI/EnemyUIComponent.cpp
+++ Source/Blur_ARPG_Warrior/Private/Components/UI/EnemyUIComponent.cpp
@@ -3,3 +3,23 @@
 
 #include "Components/UI/EnemyUIComponent.h"
 
+#include "Widgets/WarriorWidgetBase.h"
+
+void UEnemyUIComponent::RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister)
+{
+	if (EnemyDrawnWidgets.Contains(InWidgetToRegister)) return;
+	
+	EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
+
+void UEnemyUIComponent::RemoveEnemyDrawnWidgetsIfAny()
+{
+	if (EnemyDrawnWidgets.IsEmpty()) return;
+
+	for (UWarriorWidgetBase* DrawnWidget : EnemyDrawnWidgets)
+	{
+		if (!DrawnWidget) continue;
+
+		DrawnWidget->RemoveFromParent();
+	}
+}
--- Source/Blur_ARPG_Warrior/Public/Components/UI/EnemyUIComponent.h
+++ Source/Blur_ARPG_Warrior/Public/Components/UI/EnemyUIComponent.h
@@ -6,12 +6,24 @@
 #include "Components/UI/PawnUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
-/**
- * 
- */
+class UWarriorWidgetBase;
+
+//敌人UI组件，用于管理和清空Widgets。
 UCLASS()
 class BLUR_ARPG_WARRIOR_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
+
+public:
+	/// 注册Widget到UI组件。
+	/// @param InWidgetToRegister 
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister);
+
+	/// 移除所有敌人绘制的UI。
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
 	
+private:
+	TArray<UWarriorWidgetBase*> EnemyDrawnWidgets;
 };
